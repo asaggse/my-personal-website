9fd6b77771dfae1598e1934c424d5bd99d1fba41
Alessandro Saggese <asaggese@gmail.com>
2023-02-01 23:30:40 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="social-links">
+        <a href="https://github.com/asaggse">
+          <i class="devicon-github-original"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/alessandro-saggese/">
+          <i class="devicon-linkedin-plain"></i>
+        </a>
+      </div>
+      <p>Copyright &copy; Alessandro Saggese 2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
