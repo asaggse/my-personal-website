22b8c53288c911d0a73b1be73a428ca3a520d18e
Alessandro Saggese <asaggese@gmail.com>
2023-02-01 23:30:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header"
+import Home from "./components/Home"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Footer from "./components/Footer"
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      {/* <Footer /> */}
+      {/* Add other components here */}
     </div>
   );
 }
