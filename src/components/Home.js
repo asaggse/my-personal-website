35ce8a3fdfbf900c8bbe68e2ef6d50667e42ee95
Alessandro Saggese <asaggese@gmail.com>
2023-02-01 23:30:28 +0100

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+import profilePic from "./profile-pic.jpg";
+
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="home-container">
+        <div className="home-text">
+          <h2>Hi, I'm Alessandro Saggese</h2>
+          <h3>Welcome to my website</h3>
+          <p>Scroll down ⬇</p>
+        </div>
+        <div className="Home-pic">
+          <img src={profilePic} alt="Profile Picture" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
