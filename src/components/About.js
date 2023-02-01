7a8483487028ddb0f93f84533a34b265e075d31a
Alessandro Saggese <asaggese@gmail.com>
2023-02-01 23:30:49 +0100

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <section id="aboutme">
+      <div className="about-container">
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p>
+            Hi, I'm Alessandro Saggese and I'm a developer with a passion for solving problems and creating innovative solutions.
+            I'm always learning and striving to improve my skills, and I'm excited to be a part of the development community.
+            I believe that collaboration and continuous learning are key to success in this field, and I'm eager to work with others to build something great.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
