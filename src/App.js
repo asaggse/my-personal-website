687f348221a196f4406c14dc0e3fc0f70afd67d7
Alessandro Saggese <asaggese@gmail.com>
2023-02-02 21:10:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header"
 import Home from "./components/Home"
 import About from "./components/About"
@@ -6,16 +6,38 @@ import Skills from "./components/Skills"
 import Footer from "./components/Footer"
 import "./App.css";
 
-
 function App() {
+  const [showGoToTop, setShowGoToTop] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 100) {
+      setShowGoToTop(true);
+    } else {
+      setShowGoToTop(false);
+    }
+  };
+
+  const handleGoToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="App">
       <Header />
       <Home />
       <About />
       <Skills />
-      {/* <Footer /> */}
-      {/* Add other components here */}
+      {showGoToTop &&
+        <button className="go-to-top" onClick={handleGoToTop}>⬆</button>
+      }
+      <Footer />
     </div>
   );
 }
