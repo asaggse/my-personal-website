7b4823064f00dbbdca95e919eb4ea7b974553830
Alessandro Saggese <asaggese@gmail.com>
2023-02-03 20:00:41 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,15 @@ function Footer() {
   return (
     <footer>
       <div className="social-links">
+      <p>Seguimi su:&ensp;</p>
         <a href="https://github.com/asaggse" target="_blank" rel="noreferrer">
           <i class="devicon-github-original"></i>
         </a>
         <a href="https://www.linkedin.com/in/alessandro-saggese/" target="_blank" rel="noreferrer">
           <i class="devicon-linkedin-plain"></i>
+        </a>
+        <a href="https://twitter.com/asaggese_" target="_blank" rel="noreferrer">
+          <i class="devicon-twitter-original"></i>
         </a>
       </div>
       <p>Copyright &copy; Alessandro Saggese 2023</p>
