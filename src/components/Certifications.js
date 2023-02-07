8efa05fc11e408e2685947819751467f6610e9ea
Alessandro Saggese <asaggese@gmail.com>
2023-02-07 16:52:42 +0100

    Update Certifications.js

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -2,49 +2,76 @@ import React, { useState } from 'react';
 import "./Certifications.css";
 
 const Certifications = () => {
-  const [certifications] = useState([
-    {
-      title: 'Certification 1',
-      description: 'Description for certification 1',
-      image: require('./images/150x150-2-500x500.png'),
-    },
-    {
-      title: 'Certification 2',
-      description: 'Description for certification 2',
-      image: require('./images/150x150-2-500x500.png'),
-    },
-    {
-      title: 'Certification 3',
-      description: 'Description for certification 3',
-      image: require('./images/150x150-2-500x500.png'),
-    },
-  ]);
+    const [certifications] = useState([
+        {
+            title: 'Front End Libraries',
+            description: 'freeCodeCamp',
+            image: require('./images/150x150-2-500x500.png'),
+        },
+        {
+            title: 'Responsive Web Design',
+            description: 'freeCodeCamp',
+            image: require('./images/150x150-2-500x500.png'),
+        },
+        {
+            title: 'Boolean Coding Week',
+            description: 'Boolean',
+            image: require('./images/150x150-2-500x500.png'),
+        },
+    ]);
 
-  const [zoom, setZoom] = useState(false);
+    const [zoom, setZoom] = useState({
+        show: false,
+        title: '',
+        image: ''
+    });
 
-  const handleClick = () => {
-    setZoom(!zoom);
-  };
+    const handleClick = (title, image) => {
+        setZoom({
+            show: true,
+            title,
+            image
+        });
+    };
 
-  return (
-    <section id="certifications">
-      <h2>Certifications</h2>
-      <ul>
-        {certifications.map((certification, index) => (
-          <li key={index}>
-            <h3>{certification.title}</h3>
-            <img
-              src={certification.image}
-              alt={certification.title}
-              onClick={handleClick}
-              style={{ cursor: 'pointer' }}
-            />
-            <p>{certification.description}</p>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+    const handleClose = () => {
+        setZoom({
+            show: false,
+            title: '',
+            image: ''
+        });
+    };
+
+    return (
+        <section id="certifications">
+            <h2>Certifications</h2>
+            <ul>
+                {certifications.map((certification, index) => (
+                    <li key={index}>
+                        <h3>{certification.title}</h3>
+                        <img
+                            src={certification.image}
+                            alt={certification.title}
+                            onClick={() => handleClick(certification.title, certification.image)}
+                            style={{ cursor: 'pointer' }}
+                            title="Click to zoom"
+                        />
+                        <p>by {certification.description}</p>
+                    </li>
+                ))}
+            </ul>
+            {zoom.show && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <div className='modal-header'>
+                            <span className="close" onClick={handleClose}>×</span>
+                        </div>
+                        <img src={zoom.image} alt={zoom.title} />
+                    </div>
+                </div>
+            )}
+        </section>
+    );
 };
 
 export default Certifications;
