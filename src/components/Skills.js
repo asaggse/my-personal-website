2a1b7f3d2d9a09ac2c97caef6bc8da360bd6b67a
Alessandro Saggese <asaggese@gmail.com>
2023-03-06 00:35:26 +0100

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import "./Skills.css";
 
+const icons = [
+    { name: 'html5', type:'plain-wordmark', color: 'colored' },
+    { name: 'css3', type:'plain-wordmark', color: 'colored' },
+    { name: 'javascript', type:'plain', color: 'colored' },
+    { name: 'react-original', type:'wordmark', color: 'colored' },
+    { name: 'bootstrap', type:'plain-wordmark', color: 'colored' },
+    { name: 'github-original', type:'wordmark', color: 'colored' },
+];
+
 const Skills = () => {
     return (
         <section id="skills">
             <div className="skills-container">
                 <h2>Skills</h2>
                 <div className="skills-grid">
-                    <div className="skills-grid-item">
-                        <i class="devicon-html5-plain-wordmark colored"></i>
-                    </div>
-                    <div className="skills-grid-item">
-                        <i class="devicon-css3-plain-wordmark colored"></i>
-                    </div>
-                    <div className="skills-grid-item">
-                        <i className="devicon-javascript-plain colored"></i>
-                    </div>
-                    <div className="skills-grid-item">
-                        <i class="devicon-react-original-wordmark colored"></i>
-                    </div>
-                    <div className="skills-grid-item">
-                        <i class="devicon-github-original-wordmark colored"></i>
-                    </div>
+                    {icons.map(({ name, type, color }) => (
+                        <div key={name} className="skills-grid-item">
+                            <i className={`devicon-${name}-${type} ${color}`} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
