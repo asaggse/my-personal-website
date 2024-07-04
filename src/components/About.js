eea1192b74b17289fefc2dd83a4a17a26c760aa8
Alessandro Saggese <asaggese@gmail.com>
2024-07-04 19:00:35 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,10 @@ const About = () => {
         <div className="about-text">
           <h2>About Me</h2>
           <p>
-            I'm a developer with a passion for solving problems and creating innovative solutions.
-            I'm always learning and striving to improve my skills, and I'm excited to be a part of the development community.
-            I believe that collaboration and continuous learning are key to success in this field, and I'm eager to work with others to build something great.
+          I'm a developer and **UX/UI designer** with a passion for solving problems and creating innovative solutions.
+          I'm always learning and striving to improve my skills, and I'm excited to be a part of the development community.
+          I believe that collaboration and continuous learning are key to success in this field, and I'm eager to work with others to build something great.
+          As a UX/UI designer, I also bring a keen eye for design and user experience, ensuring that the solutions I create are not only functional, but also intuitive and user-friendly.
           </p>
         </div>
       </div>
